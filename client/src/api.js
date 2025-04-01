d913b2d2ac7d5ae1c129affa6dd9e4f840ea1bdf
Shambhavi Anand <125148718+shambhavi-anand01@users.noreply.github.com>
2025-04-01 14:39:07 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:5000/api";
+export const BASE_URL = "https://resume-with-ai.onrender.com/api";
 // export const BASE_URL = "https://resume-builder-zkad.onrender.com/api"
 //export const BASE_URL = "https://resume-builder-mern-one.vercel.app/api"
